3ab3645ff1e529dd5c53bf8c94bd90d8a5366d9a
Bruno Leon <bruno.leon@savoirfairelinux.com>
2011-12-22 10:39:06 -0500

    Do not force network mask for exported networks !!!

--- manifests/exportedmynetworks.pp
+++ manifests/exportedmynetworks.pp
@@ -4,6 +4,6 @@ define postfix::exportedmynetworks(
   concat::fragment{ "postfixexportedmynetworks_${address}" :
     target  => '/etc/postfix/exported_mynetworks',
     order   => 50,
-    content => "${address}/32\n"
+    content => "${address}\n"
   }
 }
