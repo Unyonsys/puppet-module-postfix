ecc9bfa45c1fa39f99036140c5bf2f75bbc2badb
Bruno Léon <bruno.leon@unyonsys.com>
2012-03-12 22:52:42 +0100

    Fix dynamic lookup variable warning message

--- manifests/init.pp
+++ manifests/init.pp
@@ -28,36 +28,36 @@ class postfix (
   }
 
   class { 'postfix::install':
-    postfix_smtpd_tls    => $postfix_smtpd_tls,
-    postfix_ldap_support => $postfix_ldap_support,
-    use_greylisting      => $use_greylisting
+    postfix_smtpd_tls    => $postfix::postfix_smtpd_tls,
+    postfix_ldap_support => $postfix::postfix_ldap_support,
+    use_greylisting      => $postfix::use_greylisting
   }
 
   class { 'postfix::config':
-    managed_mail_domains             => $managed_mail_domains,
-    root_mail_alias                  => $root_mail_alias,
-    postfix_mydestination_complement => $postfix_mydestination_complement,
-    postfix_relayhost                => $postfix_relayhost,
-    postfix_sasl_type                => $postfix_sasl_type,
-    postfix_message_size_limit       => $postfix_message_size_limit,
-    postfix_smtpd_tls                => $postfix_smtpd_tls,
-    tls_chain                        => $tls_chain,
-    tls_key                          => $tls_key,
-    postfix_ldap_support             => $postfix_ldap_support,
-    authentication_ldap_servers      => $authentication_ldap_servers,
-    ldap_bind_dn                     => $ldap_bind_dn,
-    ldap_bind_pw                     => $ldap_bind_pw,
-    accounts_query_filter            => $accounts_query_filter,
-    aliases_query_filter             => $aliases_query_filter,
-    result_attribute                 => $result_attribute,
-    use_greylisting                  => $use_greylisting,
-    amavis                           => $amavis
+    managed_mail_domains             => $postfix::managed_mail_domains,
+    root_mail_alias                  => $postfix::root_mail_alias,
+    postfix_mydestination_complement => $postfix::postfix_mydestination_complement,
+    postfix_relayhost                => $postfix::postfix_relayhost,
+    postfix_sasl_type                => $postfix::postfix_sasl_type,
+    postfix_message_size_limit       => $postfix::postfix_message_size_limit,
+    postfix_smtpd_tls                => $postfix::postfix_smtpd_tls,
+    tls_chain                        => $postfix::tls_chain,
+    tls_key                          => $postfix::tls_key,
+    postfix_ldap_support             => $postfix::postfix_ldap_support,
+    authentication_ldap_servers      => $postfix::authentication_ldap_servers,
+    ldap_bind_dn                     => $postfix::ldap_bind_dn,
+    ldap_bind_pw                     => $postfix::ldap_bind_pw,
+    accounts_query_filter            => $postfix::accounts_query_filter,
+    aliases_query_filter             => $postfix::aliases_query_filter,
+    result_attribute                 => $postfix::result_attribute,
+    use_greylisting                  => $postfix::use_greylisting,
+    amavis                           => $postfix::amavis
   }
   class { 'postfix::service':
-    use_greylisting => $use_greylisting
+    use_greylisting => $postfix::use_greylisting
   }
 
-  postfix::alias { 'root':          destination => $root_mail_alias }
+  postfix::alias { 'root':          destination => $postfix::root_mail_alias }
   postfix::alias { 'postmaster':    destination => 'root' }
   postfix::alias { 'mailer-daemon': destination => 'root' }
 }
